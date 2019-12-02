aa3f2938a9b58b4c3fd21d244507063284b93005
Wu-Chong <wu.chong.lin@iisgalvanimi.edu.it>
2019-12-02 18:48:04 +0000

    3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const lego = require('./Lego.json');
 var app = express(); 
 
 app.set('view engine', 'pug');
-
+app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
   res.render('index',{
    title: 'Homepage',
@@ -16,4 +16,4 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
-app.use(express.static(__dirname + '/public')); // Dico ad express dove recuperare i file statici
+
